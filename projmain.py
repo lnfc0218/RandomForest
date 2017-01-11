95251b68cff352d3d90a9168e384a1aff60b0e48
Oktay Bahceci <oktaybahceci@Oktays-MacBook-Pro.local>
2017-01-11 23:37:01 +0100

    nvm

--- projmain.py
+++ projmain.py
@@ -8,10 +8,10 @@ p.set_option('display.max_row', 1000)
 class GlassSample:
     "Represenation of data samles"
 
-    def __init__(self, identity, values, type):
-        self.identity = identity
+    def __init__(self, sample_class, values, identity):
+        self.sample_class = sample_class
         self.attribute = dict(zip(glass_attributes, values))
-        self.type = type
+        self.identity = identity
 
 
 class Attribute:
@@ -25,15 +25,15 @@ class Attribute:
         return self.name
 
 
-glass_attributes = (Attribute('RI', tuple(numpy.linspace(1.5112,1.5339).tolist())),
-              Attribute('Na', tuple(numpy.linspace(10.73,17.38).tolist())),
-              Attribute('Mg', tuple(numpy.linspace(0 , 4.49).tolist())),
-              Attribute('Al', tuple(numpy.linspace(0.29, 3.5).tolist())),
-              Attribute('Si', tuple(numpy.linspace(69.81, 75.41).tolist())),
-              Attribute('K', tuple(numpy.linspace(0, 6.21).tolist())),
-              Attribute('Ca', tuple(numpy.linspace(5.43, 16.19).tolist())),
-              Attribute('Ba', tuple(numpy.linspace(0, 3.15).tolist())),
-              Attribute('Fe', tuple(numpy.linspace(0, 0.51).tolist())))
+glass_attributes = (Attribute('A1', tuple(numpy.linspace(1.5112,1.5339).tolist())),
+              Attribute('A2', tuple(numpy.linspace(10.73,17.38).tolist())),
+              Attribute('A3', tuple(numpy.linspace(0 , 4.49).tolist())),
+              Attribute('A4', tuple(numpy.linspace(0.29, 3.5).tolist())),
+              Attribute('A5', tuple(numpy.linspace(69.81, 75.41).tolist())),
+              Attribute('A6', tuple(numpy.linspace(0, 6.21).tolist())),
+              Attribute('A7', tuple(numpy.linspace(5.43, 16.19).tolist())),
+              Attribute('A8', tuple(numpy.linspace(0, 3.15).tolist())),
+              Attribute('A9', tuple(numpy.linspace(0, 0.51).tolist())))
 
 
 def read_glass_dataset(path):
@@ -60,7 +60,7 @@ def read_glass_dataset(path):
     print structured_data
     samples = ()
     for item in structured_data:
-      samples += (GlassSample(item[2], item[1], item[0]),)
+      samples += (GlassSample(item[0], item[1], item[2]),)
 
     return samples
  
@@ -68,4 +68,4 @@ def read_glass_dataset(path):
 # Example for glass dataset
 samples = read_glass_dataset(path)
 for item in samples:
-  print item.identity, item.attribute, item.type
+  print item.sample_class, item.attribute, item.identity
